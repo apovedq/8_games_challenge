8f5b187784545ff651fd13c93601fc6c374f9d00
apovedq <povedandres11@gmail.com>
2021-04-30 09:55:50 -0500

    Images Fixed (Ejercicio 1)

--- Cambio de usuario/Character.js
+++ Cambio de usuario/Character.js
@@ -1,7 +1,10 @@
 class Character {
     
     constructor(int) {
-       
+        this.user = loadImage('Assets/first.png');
+        this.user1 = loadImage('Assets/second.png');
+        this.user2 = loadImage('Assets/third.png');
+        this.user3 = loadImage('Assets/forth.png');
         this.x = 0;
         this.y = 0;
         this.int = int;
@@ -14,26 +17,22 @@ class Character {
             case 1:
                 this.x = 100;
                 this.y = 250;
-                fill(0,255,40);
-                circle(this.x, this.y, 100);
+                image(this.user, this.x, this.y);
                 break;
             case 2:
                 this.x = 200;
                 this.y = 250;
-                fill(255,0,0);
-                circle(this.x, this.y, 100);
+                image(this.user1, this.x, this.y);
                 break;
             case 3:
                 this.x = 300;
                 this.y = 250;
-                fill(255,255,0);
-                circle(this.x, this.y, 100);
+                image(this.user2, this.x, this.y);
                 break;
             case 4:
                 this.x = 400;
                 this.y = 250;
-                fill(255,0,255);
-                circle(this.x, this.y, 100);
+                image(this.user3, this.x, this.y);
                 break;
         }
     }
